6adf0122b677748a1eed0704b901b504cd3136be
Oleksandr Arshynov <oleksandrarshynov@gmail.com>
2024-03-14 16:34:10 +0200

    add changes in forms.py

--- django_project/quotes/forms.py
+++ django_project/quotes/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Author, Quote
+from .models import Author, Quote, Tag
 
 class AuthorForm(forms.ModelForm):
     class Meta:
@@ -7,6 +7,15 @@ class AuthorForm(forms.ModelForm):
         fields = ['fullname', 'born_date','born_location', 'description'] 
 
 class QuoteForm(forms.ModelForm):
+    
+    author = forms.ModelChoiceField(queryset=Author.objects.all(), empty_label=None, to_field_name='id', label='Author')
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple, label='Tags')
+
     class Meta:
         model = Quote
         fields = ['author', 'quote', 'tags'] 
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['author'].label_from_instance = lambda obj: obj.fullname
+        self.fields['tags'].label_from_instance = lambda obj: obj.name
